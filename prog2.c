8296955f7806302af062ce08860b4edbefb93749
Gunashrees456 <gumashree.s@s.amity.edu>
2025-03-08 19:17:02 +0530

    start the program

--- prog2.c
+++ prog2.c
@@ -1,19 +1,1 @@
 #include<stdio.h>
-
-int main(){
-	int a.b,temp;
-
-	printf("enter two numbers:");
-	scanf("%d %d",&a,&b);
-
-	temp=a;
-	a=b;
-	b=temp;
-
-	printf("after swapping: a = %d, b = %d\n",a,b);
-
-	return 0;
-}
-
-
-	
