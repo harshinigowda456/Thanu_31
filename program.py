28e7d0ac292709698aad3f999b56f28e376a37e1
Gunashrees456 <gumashree.s@s.amity.edu>
2025-03-08 21:47:30 +0530

    calculation and result

--- program.py
+++ program.py
@@ -2,3 +2,4 @@ P=float(input("enter the principal amount:"))
 R=float(input("enter the rate of interest:))
 T=float(input("enter the time:"))
 simple_interest=(P*R*T)/100
+print(f"The simple interest is:{simple_interest}")
